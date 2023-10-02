8c4b21fa4164dc751714459dacc4e8d4ad8dc0e8
KatyaKhudoyarova <114654457+KatyaKhudoyarova@users.noreply.github.com>
2023-10-02 18:21:45 +0300

    Add files via upload

---
+++ 7task.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>
+
+
+using namespace std;
+
+int main() {
+    float x=2.3 , y=2.3 , z=2.123456 , v=2.123456, k=2.123456 , a=2.123456;
+    cout<< fixed;
+    cout<<setprecision(1)<<x<<""<<endl<<setprecision(2)<<y<<""<<endl<<setprecision(2)<<k<<""<<endl<<setprecision(6)<<z<<""<<endl<<setprecision(0)<<a<<endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
